d1cb6464ed36319e47ffc88a6b1e84f20046aec0
jacksonlevine <jack@netbug.us>
2023-04-21 09:44:33 -0700

    Break bag list off into its own component

---
+++ src/components/BagList.js
@@ -0,0 +1,16 @@
+import BagEntry from "./BagEntry"
+
+function BagList(props)
+{
+  <div className="bagList">
+            {this.state.bags.map(
+                (bag, index) =>
+                    <BagEntry
+                        name = {bag.name}
+                        origin = {bag.origin}
+                        price = {bag.price}
+                        roast = {bag.roast}
+                        key = {index}/>
+            )}
+  </div>
+}
