1ca92af79dd1bd716f5f05dec7e3fe9e1f9950ce
jacksonlevine <jack@netbug.us>
2023-04-21 09:31:56 -0700

    add BagEntry jsx

---
+++ src/components/BagEntry.js
@@ -0,0 +1,9 @@
+function BagEntry(props)
+{
+  <div className="bagEntry">
+    <h3>{props.name}</h3>
+    <h3>{props.origin}</h3>
+    <h3>{props.price}</h3>
+    <h3>{props.roast}</h3>
+  </div>
+}
